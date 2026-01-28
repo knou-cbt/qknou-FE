fda101f8998d8813b210f6b55dc3a7964ef2b502
esjeong <esjeong@kephas.kr>
2026-01-28 16:33:30 +0900

    FIX: keyword에 큐노 검색어 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,7 +20,7 @@ const geistMono = Geist_Mono({
 const SITE_NAME = "QKNOU";
 const SITE_URL = "https://www.qknou.kr";
 const SITE_DESC =
-  "QKNOU는 방송통신대학교(KNOU) 기출문제를 한곳에서 제공하는 문제 풀이 플랫폼입니다. 과목/연도별 문제 풀이와 학습을 지원합니다.";
+  "QKNOU(큐노)는 방송통신대학교(KNOU) 기출문제를 한곳에서 제공하는 문제 풀이 플랫폼입니다. 과목/연도별 문제 풀이와 학습을 지원합니다.";
 const OG_IMAGE = `${SITE_URL}/mobile_preview.png`;
 
 /**
@@ -58,6 +58,7 @@ export const metadata: Metadata = {
 
   keywords: [
     "QKNOU",
+    "큐노",
     "방송통신대학교",
     "KNOU",
     "방송대",
